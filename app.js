2ac110ba795a16a514d183cf30bf09e59c0b5e92
valentinalonso <alonsovalen6@gmail.com>
2021-12-22 14:46:32 -0300

    heroku

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ app.use(express.static(publicPath) );
 app.use(express.static(viewsPath) );
 
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo');
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
 })
-
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'))
 });
