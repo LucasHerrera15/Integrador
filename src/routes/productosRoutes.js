3d3ed2da66d3aa6aad11209f802c92b053b70e19
valentinalonso <alonsovalen6@gmail.com>
2022-01-26 02:12:08 -0300

    add creacion de producto front

--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -9,9 +9,9 @@ router.get('/carrito', productosControllers.carrito);
 router.get('/detalleProducto/:id', productosControllers.detalleProducto);
 router.get('/listadoProducto', productosControllers.listadoProducto);
 router.get('/creacionProducto', productosControllers.creacionProducto);
-router.get('/:id/edit'), productosControllers.editarProducto);
-router.put('/:id'), productosControllers.updateProducto);
-router.delete('/:id'), productosControllers.eliminarProducto);
+router.get('/:id/edit', productosControllers.editarProducto);
+router.put('/:id', productosControllers.updateProducto);
+router.delete('/:id', productosControllers.eliminarProducto);
 
 
 
