4c6b826b00f3efadad9eedaefbd5fc0cf2530657
valentinalonso <alonsovalen6@gmail.com>
2022-01-28 17:56:07 -0300

    add accion de creacion y listado de productos

--- src/controllers/productosControllers.js
+++ src/controllers/productosControllers.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../database/productosbase.json');
-/* const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); */
+const productosFilePath = path.join(__dirname, '../database/dataProductos.json');
+const productos = JSON.parse(fs.readFileSync(productosFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -17,15 +17,47 @@ const productosControllers =
         res.render('products/carrito');
     },
     detalleProducto: (req, res) => {
-        res.render('products/detalleProducto');
+        let idProductoSeleccionado = req.params.id;
+		let productoSeleccionado;
+
+		for (let p of productos){
+
+			if(p.id==idProductoSeleccionado){
+				productoSeleccionado=p;
+				break;
+			}
+		}
+        res.render('products/detalleProducto', {productos: productoSeleccionado});
     },
 
     listadoProducto: (req, res) => {
-        res.render('products/listadoProducto', {p: products});
+        res.render('products/listadoProducto', {p: productos});
     },
+    // Crear Producto pagina
     creacionProducto: (req, res) => {
         res.render('products/creacionProducto');
     },
+    // Crear Producto
+    crear: (req, res) => {
+		
+		let nuevoID=(productos[productos.length-1].id)+1 
+		
+		let productoNuevo = {
+			id: nuevoID,
+			name: req.body.producto,
+			description: req.body.descripcion,
+			price: req.body.precio,
+			discount: req.body.descuento,
+            talle: req.body.talle,
+			image: req.file.filename,
+			category: "En stock"		
+		}
+		productos.push(productoNuevo)
+
+		fs.writeFileSync(productosFilePath, JSON.stringify(productos,null,' '));
+
+		res.redirect('/');
+	},
     editarProducto: (req, res) => {
         res.render('products/editarProducto');
     },
--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -2,13 +2,27 @@ const productosControllers = require('./../controllers/productosControllers');
 
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 
+const multerDiskStorage = multer.diskStorage({
+    destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
+     cb(null, path.join(__dirname,'../../public/images/products'));    // Ruta donde almacenamos el archivo
+    },
+    filename: function(req, file, cb) {          // request, archivo y callback que almacena archivo en destino
+     let imagenproducto = Date.now() + path.extname(file.originalname);   // milisegundos y extensión de archivo original
+     cb(null, imagenproducto);         
+    }
+});
+
+const uploadFile = multer({ storage: multerDiskStorage });
 
 router.get('/', productosControllers.listadoProducto);
 router.get('/carrito', productosControllers.carrito);
-router.get('/detalleProducto', productosControllers.detalleProducto);
+router.get('/detalleProducto/:id', productosControllers.detalleProducto);
 router.get('/listadoProducto', productosControllers.listadoProducto);
 router.get('/creacionProducto', productosControllers.creacionProducto);
+router.post('/creacionProducto', uploadFile.single('imagenproducto') ,productosControllers.crear); 
 router.get('/:id/edit', productosControllers.editarProducto);
 router.put('/:id', productosControllers.updateProducto);
 router.delete('/:id', productosControllers.eliminarProducto);
