4de48a592646aef4823b7aedea3ca1440683f099
Your Name <you@example.com>
2022-01-30 10:31:02 -0300

    Edición de producto(No terminado)

--- src/controllers/productosControllers.js
+++ src/controllers/productosControllers.js
@@ -1,13 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../database/productosbase.json');
-/* const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); */
+const productsFilePath = path.join(__dirname, '../database/dataProductos.json');
+const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
-
-
 
 
 
@@ -26,9 +23,41 @@ const productosControllers =
     creacionProducto: (req, res) => {
         res.render('products/creacionProducto');
     },
+
     editarProducto: (req, res) => {
-        res.render('products/editarProducto');
+        let idProductoSeleccionado = req.params.id;
+		let productoSeleccionado;
+
+		for (let p of productos){
+
+			if(p.id==idProductoSeleccionado){
+				productoSeleccionado=p;
+				break;
+			}
+		}
+		res.render('editarProducto',{producto: productoSeleccionado});
     },
+    guardarEdición: (req, res) => {
+        let idProducto = req.params.id;
+        let datos = req.body;
+
+        for(let p of productos){
+            if(p.id==idProducto){
+				p.nombre = datos.nombre;
+                p.descripcion = datos.descripcion;
+                p.imagen = datos.imagen;
+				p.precio = datos.precio;
+                p.talle = datos.talle
+				p.descuento = datos.descuento;
+				p.estado = datos.estado
+				break;
+			}
+		}
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(productos,null,' '));
+
+	    res.redirect('/detalleProducto');
+    },    
     eliminarProducto: (req, res) => {
         res.render('products/eliminarProducto');
     },
--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -9,8 +9,11 @@ router.get('/carrito', productosControllers.carrito);
 router.get('/detalleProducto', productosControllers.detalleProducto);
 router.get('/listadoProducto', productosControllers.listadoProducto);
 router.get('/creacionProducto', productosControllers.creacionProducto);
-router.get('/:id/edit', productosControllers.editarProducto);
-router.put('/:id', productosControllers.updateProducto);
+//Editar productos
+router.get('/edit/:id', productosControllers.editarProducto);
+router.put('/edit/:id', productosControllers.updateProducto);
+
+
 router.delete('/:id', productosControllers.eliminarProducto);
 
 
