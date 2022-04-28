fe378c9c467debf50b5a0e3849eef74aa7d87c7e
valentinalonso <alonsovalen6@gmail.com>
2022-04-28 19:23:37 -0300

    fix estetica add search

--- public/js/menuMobile.js
+++ public/js/menuMobile.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector(".menu__icon");
 const navMenu = document.querySelector(".navigation");
+const icon = document.getElementById("icon")
 
 navToggle.addEventListener("click", () =>{
     navMenu.classList.toggle("navigation_visible");
